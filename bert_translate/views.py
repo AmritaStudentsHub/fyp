9eb3cab8daf4824c6146836d444179499cc902d4
prahladkumarpopuri <prahladkumar2222@gmail.com>
2021-05-18 15:57:33 +0530

    changes to html and views

--- bert_translate/views.py
+++ bert_translate/views.py
@@ -2,20 +2,26 @@ from django.shortcuts import render, redirect
 from .forms import QuestionForm
 from .BERTSQuADmaster.bert import QA
 from .BERTSQuADmaster.translate import englishToHindiTranslate
-
+from englisttohindi.englisttohindi import EngtoHindi
 # Create your views here.
 def basic(request):
     if request.method =='POST':
-        question = request.POST['question']
+        question1 = request.POST['question1']
+        question2 = request.POST['question2']
         para = request.POST['paragraph']
-        print(question)
+        print(question1)
         model = QA("/home/prahlad/Desktop/final FYP/bert_translate/BERTSQuADmaster/model/")
-        english_text = model.predict(para,question)["answer"]
-        print(english_text)
+        english_text1 = model.predict(para,question1)["answer"]
+        english_text2 = model.predict(para,question2)["answer"]
+        print(english_text1)
         # translated_text = englishToHindiTranslate(english_text)
         # print(translated_text)
         # form = QuestionForm(initial={'input_4':para})
-        return render(request,'Feedback_Form.html',{'para':para,'question':question,'ans':english_text})
+        ans1_t = EngtoHindi(english_text1)
+        ans2_t = EngtoHindi(english_text2)
+        # ans1_t = '१८ मई १८८२'
+        # ans2_t ='१९२०-२१ और १९५९-६०'
+        return render(request,'Feedback_Form.html',{'para':para,'question1':question1,'ans1':english_text1,'ans1t':ans1_t.convert,'question2':question2,'ans2':english_text2,'ans2t':ans2_t.convert})
     else:
         form = QuestionForm()
     return render(request,'Feedback_Form.html',{'form':form})
